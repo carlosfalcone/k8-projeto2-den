4d72a54160376dc72a76025b5d0fe32d469a6b5e
Carlos Falcone <carloseduardo.falcone@gmail.com>
2024-01-29 16:23:14 -0300

    cicd 9

--- script-delete.sh
+++ script-delete.sh
@@ -2,11 +2,14 @@
 echo "Deletando os servicos..."
 kubectl delete service mysql-connection
 kubectl delete service app
+echo "Conferindo se os services foram removidos"
 kubectl get services
 
 
 echo "Deletando o deployment..."
 kubectl delete deploy mysql
 kubectl delete deploy app
+echo "Conferindo se os deployments foram removidos"
 kubectl get deploy
+echo "Conferindo se os pods foram removidos"
 kubectl get pods
--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 kubectl apply -f mysql-deployment.yml --record
-kubectl apply -f mysql-init-script.yaml --record
+kubectl apply -f mysql-init-script.yml --record
 
 kubectl apply -f app-deployment.yml --record
 
