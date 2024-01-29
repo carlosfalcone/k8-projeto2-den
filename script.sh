aadce623e1c757b56eb7d20a5ea29bb2d01ff6a9
Carlos Falcone <carloseduardo.falcone@gmail.com>
2024-01-29 15:53:37 -0300

    cicd 5

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kubectl apply -f /mysql-deployment.yml --record
-kubectl apply -f /app-deployment.yml --record
+kubectl apply -f mysql-deployment.yml --record
+kubectl apply -f app-deployment.yml --record
 
 
